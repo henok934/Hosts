3f38913048c921a8a7d5c0ab20c3476ff5232281
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2024-10-17 11:33:16 -0400

    fixed

--- myproje/venv/bin/activate.fish
+++ myproje/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/henok/eee/Dbus/myproje/venv"
+set -gx VIRTUAL_ENV "/home/henok/qq/Hosts/myproje/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
